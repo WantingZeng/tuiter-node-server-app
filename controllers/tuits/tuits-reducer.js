1968ca0fe059522fd4cc3159449903c990b4a5ea
wantingzeng <zeng.wan@northeastern.edu>
2022-11-06 18:37:49 -0500

    first commit

---
+++ controllers/tuits/tuits-reducer.js
@@ -0,0 +1,77 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+    createTuitThunk,
+    deleteTuitThunk,
+    findTuitsThunk,
+    updateTuitThunk
+} from "../../src/services/tuits-thunks.js";
+
+const initialState = {
+    tuits: [],
+    loading: false
+}
+
+const currentUser = {
+    "userName": "NASA",
+    "handle": "@nasa",
+    "image": "nasa.png",
+};
+const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+}
+
+const tuitsSlice = createSlice({
+
+
+                                   name: 'tuits',
+                                   initialState,
+
+                                   extraReducers: {
+                                       [findTuitsThunk.pending]:
+                                           (state) => {
+                                               state.loading = true
+                                               state.tuits = []
+                                           },
+                                       [findTuitsThunk.fulfilled]:
+                                           (state, { payload }) => {
+                                               state.loading = false
+                                               state.tuits = payload
+                                           },
+                                       [findTuitsThunk.rejected]:
+                                           (state) => {
+                                               state.loading = false
+                                           },
+                                       [deleteTuitThunk.fulfilled] :
+                                           (state, { payload }) => {
+                                               state.loading = false
+                                               state.tuits = state.tuits
+                                                   .filter(t => t._id !== payload._id)
+                                           },
+                                       [createTuitThunk.fulfilled]:
+                                           (state, { payload }) => {
+                                               state.loading = false
+                                               state.tuits.push(payload)
+                                           },
+                                       [updateTuitThunk.fulfilled]:
+                                           (state, { payload }) => {
+                                               state.loading = false
+                                               const tuitNdx = state.tuits
+                                                   .findIndex((t) => t._id === payload._id)
+                                               state.tuits[tuitNdx] = payload
+                                           }
+                                   },
+
+
+                                   reducers: {
+                                      ...
+                                   }
+                               });
+
+export const {createTuit,deleteTuit} = tuitsSlice.actions;
+export default tuitsSlice.reducer;
