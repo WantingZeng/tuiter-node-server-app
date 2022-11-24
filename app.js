3dda996110650ef6e983e7de3d1b96a70071648e
wantingzeng <zeng.wan@northeastern.edu>
2022-11-24 12:38:32 -0500

    assignment 9 created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
@@ -10,4 +11,8 @@ app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+//mongoose.connect('mongodb://localhost:27017/tuiter');
 app.listen(process.env.PORT || 4000);
